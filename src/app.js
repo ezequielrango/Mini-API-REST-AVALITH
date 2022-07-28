09f06fbedc250f3b3eb8070632371886b81c0a7b
Ezequiel Rango <ezequielrango29@gmail.com>
2022-07-28 08:40:36 -0300

    :wrench: set server, prisma-client

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ const app = express();
 const env = require('dotenv').config();
 const morgan = require('morgan');
 
+//variables
 const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
 
 app.listen(PORT, () => {
     console.log(`Server running in the port ${PORT}`);
