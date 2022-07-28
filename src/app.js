2d666ede78b1d9384161a5bc1537fc701a077473
Ezequiel Rango <ezequielrango29@gmail.com>
2022-07-28 14:33:05 -0300

    :globe_with_meridians: create v1Users

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ const morgan = require('morgan');
 
 //variables
 const PORT = process.env.PORT || 3000;
+const v1UsersRouter = require('./v1/routes/users_router');
 
 // middlewares
 app.use(express.json());
 app.use(morgan('dev'));
 
+//routes
+
+app.use('/api/v1/',v1UsersRouter );
 
 app.listen(PORT, () => {
     console.log(`Server running in the port ${PORT}`);
