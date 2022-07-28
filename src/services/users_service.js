353cac7ba260803dc5c6718ed9dc00e1dafad660
Ezequiel Rango <ezequielrango29@gmail.com>
2022-07-28 16:09:28 -0300

    :construction: started development userServices

---
+++ src/services/users_service.js
@@ -0,0 +1,47 @@
+const getAllUser = (req ,res) => {
+    try {
+        
+    } catch (err) {
+        
+    };
+};
+
+const getByIdUser = (req ,res) => {
+    try {
+        
+    } catch (err) {
+        
+    };
+};
+
+const createUser = (req ,res) => {
+    try {
+        
+    } catch (err) {
+        
+    };
+};
+
+const updateUser = (req ,res) => {
+    try {
+        
+    } catch (err) {
+        
+    };
+};
+
+const deleteUser = (req ,res) => {
+    try {
+        
+    } catch (err) {
+        
+    };
+};
+
+module.exports = {
+    getAllUser,
+    getByIdUser,
+    createUser,
+    updateUser,
+    deleteUser
+}
