e0f64e7c599a76ba1014f88895fc667a5746c422
Ezequiel Rango <ezequielrango29@gmail.com>
2022-07-31 19:26:17 -0300

    :construction: create getAllPost controller

---
+++ src/controllers/posts_controller.js
@@ -0,0 +1,25 @@
+const postServices = require('../services/posts_services');
+
+
+const getAll = async (res ,res) => {
+    const allPosts = await postServices.allPosts();
+    try {
+        if (allPosts.length === 0) {
+            res.status(404).json({
+                status : 404,
+                msg : 'Not found o doesnt exists'
+            });
+        }else{
+            res.status(200).json({
+                status : 200,
+                data : allPosts
+            });
+        }; 
+    } catch (err) {
+        res.status(500).json('Internal Server Error');
+    };
+};
+
+module.exports = {
+    getAll,
+}
