dae2388317e8727c6ebbdd05bbacabe3062af52b
Ezequiel Rango <ezequielrango29@gmail.com>
2022-08-10 18:05:29 -0300

    :construction: create service

--- src/services/posts_service.js
+++ src/services/posts_service.js
@@ -10,6 +10,15 @@ const getAll = (req ,res) => {
     };
 };
 
+const getById = (id) => {
+    try {
+        const getPost = Post.getById(Number(id))
+        return getPost;
+    } catch (error) {
+        console.log(error);
+    }
+}
 module.exports = {
     getAll,
+    getById
 }
