f337cb4186ad2d8d822d4e1ea89faff85a2ee22f
Ezequiel Rango <ezequielrango29@gmail.com>
2022-08-10 18:03:13 -0300

    :construction: create route method getById

--- src/v1/routes/posts_router.js
+++ src/v1/routes/posts_router.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const postController = require('../../controllers/posts_controller');
 
 router.get('/', postController.getAll);
-
+router.get('/:id', postController.getById);
     module.exports = router;
