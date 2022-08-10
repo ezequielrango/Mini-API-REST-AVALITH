d3c757b6656d44c927a57c021daf2a065253ebb7
Ezequiel Rango <ezequielrango29@gmail.com>
2022-08-10 18:02:12 -0300

    :construction: create controller getById

--- src/controllers/posts_controller.js
+++ src/controllers/posts_controller.js
@@ -19,7 +19,25 @@ const getAll = async (req ,res) => {
         res.status(500).json('Internal Server Error');
     };
 };
-
+const getById = async (req,res) => {
+    const {id} = req.params;
+    const getPost = await postServices.getById(id);
+    try {
+        if (!getPost) {
+            res.status(404).json({
+                msg : "Resource not found or doesn´t exist",
+                status : 404
+            });
+        }
+        res.status(200).json({
+            data : getPost,
+            status : 200
+        });
+    } catch (error) {
+        res.status(500).json('Internal Server Error');
+    };
+}
 module.exports = {
     getAll,
+    getById
 }
