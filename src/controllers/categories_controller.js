c83487ff2482bbb99c696faae2b97406800bcfec
Ezequiel Rango <ezequielrango29@gmail.com>
2022-08-17 17:39:35 -0300

    :construction: create controller getById Category

--- src/controllers/categories_controller.js
+++ src/controllers/categories_controller.js
@@ -18,6 +18,26 @@ const getAll = async ( req, res ) => {
     }
 }
 
+const getById = async (req,res ) => {
+    try {
+        const {id} = req.params;
+        const category = await categoryService.getById(id);
+        if (!category) {
+            res.status(404).json({
+                msg: 'Not found o doesnt exist',
+                status : 404
+            })
+        }
+        res.status(200).json({
+            data : category,
+            status : (200)
+        })
+    } catch (error) {
+        res.status(500).json('Internal server error')
+    }
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getById
 }
