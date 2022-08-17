730e0655f1974f81cbec66d0325a4b2c49155b7a
Ezequiel Rango <ezequielrango29@gmail.com>
2022-08-17 18:12:32 -0300

    :construction: create routes create category

--- src/v1/routes/categories_router.js
+++ src/v1/routes/categories_router.js
@@ -4,9 +4,7 @@ const categoryController = require('../../controllers/categories_controller.js')
 
 router.get('/',categoryController.getAll )
 router.get('/:id',categoryController.getById)
-// router.post('/', (req,res) => {
-//         res.json('post OK')
-//     })
+router.post('/', categoryController.create)
 // router.delete('/:id', (req,res) => {
 //         res.json('delete ok')
 //     })
